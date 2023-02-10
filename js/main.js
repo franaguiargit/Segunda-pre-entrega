51458138a10bceff20def032f71174b118b8646c
franaguiargit <franciscoaguiarparera@gmail.com>
2023-02-09 23:23:17 -0300

    Segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -24,10 +24,26 @@ class Habitacion{
         this.estado = estado
     }
 }
+
+function validarHotel(numeroDeReserva) {
+
+    return listaDeReservasHotel.find((Hotel) =>{
+        return Hotel.numeroDeReserva === numeroDeReserva
+    })
+}
+
 // Inicio de programa
 
 const listaDeReservasHotel = [new Hotel("A2BQ1","100 USD","La Cumbre Resort", "Cordoba La Cumbre salamanca121"), 
 new Hotel("12SZ1", "125 dolares", "Hotel Calamuchita", "Valle Calamuchita Los reartes 124"),
 new Hotel("LZGD24","80 USD", "Pajas blancas", "AV.Luchesse 504 Cordoba")]
     
-console.log(listaDeReservasHotel)
+let hotelesDisponibles = prompt("Ingrese el hotel que quiere reservar. Si no ingrese SALIR si quiere cerrar el programa")
+
+while(hotelesDisponibles != "SALIR"){
+    
+    const Hotel = validarHotel(hotelesDisponibles)
+
+
+    let hotelesDisponibles = prompt("Ingrese el hotel que quiere reservar. Si no ingrese SALIR si quiere cerrar el programa")
+}
