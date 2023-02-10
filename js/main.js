53303edbd4ad6ffb3be9b922c7100a7b08a4a86d
franaguiargit <franciscoaguiarparera@gmail.com>
2023-02-10 01:08:36 -0300

    Segunda pre entrega con error en el while y la funcion hayreservasDisponibles

--- js/main.js
+++ js/main.js
@@ -1,49 +1,77 @@
 class Hotel{
-    constructor(numeroDeReserva, precioHotel, nombredeHotel, direccionDeHotel){
-        this.numeroDeReserva = numeroDeReserva
-        this.precioHotel = precioHotel
-        this.nombredeHotel = nombredeHotel
-        this.direccionDeHotel = direccionDeHotel
+    constructor(numeroDeReservaHotel, precioReserva, nombredeHotel, direccionDeHotel){
+        this.numeroDeReservaHotel = numeroDeReservaHotel // Numero de reserva del hotel
+        this.precioReserva = precioReserva // precio neto del hotel
+        this.nombredeHotel = nombredeHotel // Nombre del hotel, esto facilita al usuario buscarlo en internet
+        this.direccionDeHotel = direccionDeHotel // direccion del hotel, un extra para saber la ubicacion del hotel
 
         
-        this.cantidadHabitaciones();
+        this.generarReservas();
     }
-    cantidadHabitaciones(){
-        this.habitaciones = []
+    generarReservas(){ 
+        this.reservas = []
+
         for(let i = 0; i < 50; i++){
-            const cuarto = new Habitacion(i,false)
-            this.habitaciones.push(cuarto)
+            const reserva = new reservaDeHotel(i,false)
+            this.reservas.push(reserva)
         }
     }
 }
 
-class Habitacion{
+class reservaDeHotel{ 
 
-    constructor(numerodeHabitacion, estado){
-        this.numerodeHabitacion = numerodeHabitacion
-        this.estado = estado
+    constructor(numerodeHabitacion, ocupada){
+        this.numerodeHabitacion = numerodeHabitacion // Que numero de habitacion tiene la reserva
+        this.ocupada = ocupada // Para saber si esta reservardo o no.
+
     }
 }
 
-function validarHotel(numeroDeReserva) {
+function validarHotel(numeroDeReservaHotel) {
 
     return listaDeReservasHotel.find((Hotel) =>{
-        return Hotel.numeroDeReserva === numeroDeReserva
+        return Hotel.numeroDeReservaHotel === numeroDeReservaHotel
     })
 }
 
-// Inicio de programa
+function hayreservasDisponibles(Hotel, cantidadDeReservas){
 
-const listaDeReservasHotel = [new Hotel("A2BQ1","100 USD","La Cumbre Resort", "Cordoba La Cumbre salamanca121"), 
+    const reservasDisponibles = Hotel.reservas.filter( (reserva) =>{
+        return !reservaDeHotel.ocupada
+    })
+    
+    return reservasDisponibles.length >= cantidadDeReservas
+
+}
+
+// Inicio de programa
+alert("Hoteles disponibles. (La cumbre cordoba numero de reserva: A2BQ1), (Hotel calamuchita numero de reserva: 12SZ1), (Pajas Blancas numero de reserva: LZGD24)")
+
+const listaDeReservasHotel = [
+new Hotel("A2BQ1","100 USD","La Cumbre Resort", "Cordoba La Cumbre salamanca121"), 
 new Hotel("12SZ1", "125 dolares", "Hotel Calamuchita", "Valle Calamuchita Los reartes 124"),
 new Hotel("LZGD24","80 USD", "Pajas blancas", "AV.Luchesse 504 Cordoba")]
     
 let hotelesDisponibles = prompt("Ingrese el hotel que quiere reservar. Si no ingrese SALIR si quiere cerrar el programa")
 
-while(hotelesDisponibles != "SALIR"){
+while(hotelesDisponibles !== "SALIR"){
     
-    const Hotel = validarHotel(hotelesDisponibles)
+    const hotell = validarHotel(hotelesDisponibles) // valida que el hotel existe
+
+    if(hotell !== undefined){
+
+        let cantidadDeReservas = (prompt("Ingrese la cantidad de reservas que quiere hacer"))
+
+        while(cantidadDeReservas <= 0 || !hayreservasDisponibles(hotell, cantidadDeReservas)){
+
+            let cantidadDeReservas = (prompt("Ingrese la cantidad de reservas que quiere hacer."))
+        }
+        console.log(cantidadDeReservas)
+    
+    } else{
+        alert("Numero de reserva invalido")
+    }
 
 
-    let hotelesDisponibles = prompt("Ingrese el hotel que quiere reservar. Si no ingrese SALIR si quiere cerrar el programa")
+
 }
